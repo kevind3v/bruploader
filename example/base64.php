0c42db9fb1ff42c0a08490beb3b9dd606e95dd5a
Kevin Siqueira <kevinsiqueira.dev@gmail.com>
2021-02-06 20:15:22 -0300

    first commit

---
+++ example/base64.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="./theme/style.css">
+
+<div class="form">
+    <form name="env" method="post" enctype="multipart/form-data">
+        <?php
+        require __DIR__ . "/../vendor/autoload.php";
+
+        //$image = new CoffeeCode\Uploader\Image("uploads", "images", false); //SEM PASTAS DE ANO E MÊS
+        $image = new \BrBunny\BrUploader\BrBase64("uploads", "images");
+        if ($_POST && $_POST['image']) {
+            try {
+                $upload = $image->upload($_POST['image'], $_POST['name']);
+                echo "<img src='{$upload}' width='100%'>";
+            } catch (Exception $e) {
+                echo "<p>(!) {$e->getMessage()}</p>";
+            }
+        }
+        ?>
+        <input type="text" name="name" placeholder="Image Name" required />
+        <input type="file" onchange="base64(this)" required />
+        <input type="hidden" name="image" id="image">
+        <button>Send Image</button>
+    </form>
+</div>
+
+
+<script>
+    //Generate base64 
+    function base64(element) {
+        var file = element.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            document.getElementById('image').value = reader.result;
+        }
+        reader.readAsDataURL(file);
+    }
+</script>
---
+++ src/BrBase64.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BrBunny\BrUploader;
+
+use CoffeeCode\Uploader\Uploader;
+
+class BrBase64 extends Uploader
+{
+    protected static $allowTypes = [
+        'jpg',
+        'jpeg',
+        'gif',
+        'png'
+    ];
+
+    /**
+     * @param string $data
+     * @param string $name
+     * @param boolean $removeOld
+     * @return string
+     * @throws \Exception
+     */
+    public function upload(string $data, string $name): string
+    {
+        if (!preg_match('/^data:image\/(\w+);base64,/', $data, $type)) {
+            throw new \Exception("Not a valid data from image");
+        }
+
+        $data = substr($data, strpos($data, ',') + 1);
+        $ext = strtolower($type[1]);
+
+        if (!in_array($ext, self::$allowTypes)) {
+            throw new \Exception('invalid image type');
+        }
+
+        $image = base64_decode($data, true);
+
+        if ($image === false) {
+            throw new \Exception('Generate image failed');
+        }
+
+        $this->ext = $ext;
+        $this->name($name);
+
+        if (!file_put_contents("{$this->path}/{$this->name}", $image)) {
+            throw new \Exception('Upload failed');
+        }
+
+        return "{$this->path}/{$this->name}";
+    }
+
+    /**
+     * @param string $path
+     * @return boolean
+     */
+    public static function remove(string $path): bool
+    {
+        if (file_exists($path)) {
+            unlink($path);
+            return true;
+        }
+        return false;
+    }
+}
